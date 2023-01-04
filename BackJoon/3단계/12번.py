1d2c8d27c09a43cea957289be6438e1232cd609b
094haley <094haley@gmail.com>
2023-01-04 19:52:02 +0900

    2023/01/04 학원

--- BackJoon/3단계/12번.py
+++ BackJoon/3단계/12번.py
@@ -1,7 +1,20 @@
 """
-날짜 : 2023/01/03
+날짜 : 2023/01/04
 이름 : 이해빈
 내용 : 백준 3단계 더하기 사이클
 """
 
 N = int(input())
+a = N
+count = 0
+
+while True:
+
+    b = a // 10 + a % 10
+    a = a % 10 * 10 + b % 10
+    count += 1
+
+    if a == N:
+        break
+
+print(count)
