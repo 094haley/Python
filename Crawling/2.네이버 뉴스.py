e79e0bd370219eef4ef2e73de265e3739a59666a
094haley <094haley@gmail.com>
2023-01-16 19:44:22 +0900

    2023/01/16 학원

---
+++ Crawling/2.네이버 뉴스.py
@@ -0,0 +1,60 @@
+"""
+날짜 : 2023/01/16
+이름 : 이해빈
+내용 : 파이썬 네이버 뉴스 크롤링 실습하기
+"""
+import requests as req
+from bs4 import BeautifulSoup as bs
+from openpyxl import Workbook
+
+pg = 1
+count = 1
+
+# 엑셀파일 생성, 시트 활성화
+workbook = Workbook()
+sheet = workbook.active
+
+while True:
+
+    # HTML 요청
+    url = 'https://news.naver.com/main/list.naver?mode=LS2D&sid2=230&sid1=105&mid=shm&date=20230116&page=%d' % pg
+    html = req.get(url, headers= {'User-Agent': 'Mozilla/5.0'}).text
+    #print(html)
+
+    # 문서 객체 생성
+    dom = bs(html, 'html.parser')
+
+    currnetPage = dom.select_one('#main_content > div.paging > strong').text
+
+    if pg != int(currnetPage):
+        break
+
+    # 데이터 파싱
+    tit = dom.select_one('#main_content > div.list_header.newsflash_header > h3').text
+    print('tit :', tit)
+
+    lis = dom.select('#main_content > div.list_body.newsflash_body > ul > li')
+
+    for li in lis:
+        tag_a = li.select_one('dl > dt:not(.photo) > a')
+        title = tag_a.text
+        href = tag_a['href']
+
+        print('count :', count)
+        print('title :', title.strip())
+        print('href :', href.strip())
+
+        # 엑셀 데이터 입력
+        sheet.append([title,href])
+
+        count += 1
+
+    pg += 1
+
+# 엑셀 저장 닫기
+workbook.save('C:/Users/java2/Desktop/News.xlsx')
+workbook.close()
+
+print('프로그램 종료...')
+
+
